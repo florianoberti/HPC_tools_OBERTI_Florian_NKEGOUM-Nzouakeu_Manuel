dbc3859b6eff142e7b530d1509820b103688680e
florianoberti <50593113+florianoberti@users.noreply.github.com>
2022-10-03 09:00:39 +0200

    Add files via upload

---
+++ functions.c
@@ -0,0 +1,91 @@
+#include "functions.h"
+int maxi(int a,int b)
+{
+  if(a>b) return a;
+  return b;
+}
+
+void check_matrix(double *mat, int n,int nrhs, int *flag)
+{
+    int i, j;
+    float sum;
+     
+    // flag == 2 for infinite solution
+    // flag == 3 for No solution
+    *flag = 2;
+   // get to the last row 
+   for (j=n;j<nrhs+n;j++){
+    if(mat[(n-1)*(n+nrhs)+j]!=0) {*flag = 3; break;}
+   }
+
+}
+
+/* function for exchanging two rows of
+   a matrix */
+void swap(double *mat, int row1, int row2,int n, int m)
+{
+  int max_dim = maxi(n,m);
+    for (int i = 0; i < m; i++)
+    {
+        int temp = mat[row1*max_dim+i];
+        mat[row1*max_dim+i] = mat[row2*max_dim+i];
+        mat[row2*max_dim+i] = temp;
+    }
+}
+void augmenter_matrix(double *mat ,double *a,double *b,int n,int nrhs)
+{
+    int i,j;
+    int ldb = maxi(n,nrhs);
+    double A[n][n], B[n][nrhs],C[n][n+nrhs];
+    for(i=0;i<n;i++)
+      for(j=0;j<n;j++)
+        A[i][j]=a[i*n+j];
+
+    for(i=0;i<n;i++){
+      for(j=0;j<nrhs;j++){
+        B[i][j]=b[i*ldb+j];
+        
+      }
+        }
+        // merge A and B
+    for(int i = 0; i < n; i++)
+    {
+       for(int j = 0; j < n+nrhs; j++)
+       {
+        if(j<n) C[i][j] = A[i][j];
+        else C[i][j] = B[i][j-n]; 
+       }
+    }
+    for(i=0;i<n;i++){
+      for(j=0;j<n+nrhs;j++){
+        mat[i*(n+nrhs)+j]= C[i][j]; }  }
+   
+}
+
+//m = n+ nrhs
+void Gauss_jordan(double *mat,int n ,int m, int *flag)
+{
+    int i,j,k,c;
+    int max_dim = maxi(n,m); 
+    double norm;
+    for(i=0;i<n;i++){
+        if(mat[i*(max_dim+1)]==0){ // if mat[i][i] = 0 search a row l (l>i) where mat[l][l]!=0
+          c = 1;
+            while ((i + c) < n && mat[(i+c)*max_dim+i] == 0)
+                c++;        
+            if ((i + c) == n) {
+                *flag = 1;
+                break;
+            }
+            swap(mat,i,i+c,n,m); // swap the two rows
+            }
+        for(j=0;j<n;j++){
+          if(i!=j){
+            norm = mat[j*max_dim+i]/mat[i*(max_dim+1)];
+            for(k=0;k<m;k++){
+                mat[j*max_dim+k]-=norm*mat[i*max_dim+k];
+            }
+        }
+        }
+    }
+}
